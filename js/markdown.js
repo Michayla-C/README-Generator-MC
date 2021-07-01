c302b4f4261a34fcfbf45a2241e4e70f6c25bff1
Michayla <michayla@Michaylas-MacBook-Pro.local>
2021-07-01 18:03:57 +1000

    markdown cont

--- js/markdown.js
+++ js/markdown.js
@@ -24,7 +24,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licBadge = renderLicenseBadge(license);
+  let licLink = renderLicenseLink(license);
+
+  return license != "" ? `\n## License \nThis project is licensed under ![License: ${license}](${licBadge}) \nFor more information, please visit: ${licLink}` : "";
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
