e97948d16df5c61cdf04128f4e2adcf38eda92ef
Michayla <michayla@Michaylas-MacBook-Pro.local>
2021-07-01 17:43:08 +1000

    mit function

--- js/markdown.js
+++ js/markdown.js
@@ -12,7 +12,15 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'mit':
+      return 'https://opemsource.org/license/MIT';
+      break;
+      default:
+        return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
